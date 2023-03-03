8ec9503365ba923283dd75b9f9006dad8a2bec0e
3B117151 <114204306+3B117151@users.noreply.github.com>
2023-03-03 11:35:03 +0800

    Add files via upload

---
+++ Test1.java
@@ -0,0 +1,11 @@
+public class Test1{
+	public static void main (String[] args){
+		Human human1=new Human("man",180,80);
+		Human human2=new Human("man",100,30);
+		
+		human1.printHight();
+		human2.printHight();
+		
+	}
+}
+
